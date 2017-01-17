1b4bf6381cc9482ad8aedd30c99181291f65f592
Idyllic <nikamanish007@gmail.com>
2017-01-17 11:58:51 +0530

    Add Validation to Users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
-	validates :name, presence: true
-	validates :email, presence: true
+	VAILD_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+	validates :name, presence: true, length: {maximum: 50}
+	validates :email, presence: true,
+					length: {maximum: 255}, 
+					format: {with: VAILD_EMAIL_REGEX},
+					uniqueness: { case_sensitive: false}
+
 end
