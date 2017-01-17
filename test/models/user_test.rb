c95a5a4253b94998aeb1dcc649858ccc0fafa675
Idyllic <nikamanish007@gmail.com>
2017-01-17 11:59:22 +0530

    Add Validation test cases

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -21,4 +21,37 @@ class UserTest < ActiveSupport::TestCase
   	@user.email = "    "
   	assert_not @user.valid?
   end
+
+  test "name should not be too long" do
+  	@user.name = "a"*51
+  	assert_not @user.valid?
+  end  
+
+  test "email should not be too long" do
+  	@user.email = "a"*256
+  	assert_not @user.valid?
+  end
+
+  test "email validation should  accept valid email" do
+  	valid_addresses = %w[USER@foo.COM THE_US-ER@foo.bar.org first.last@foo.jp alice+bob@baz.cn]
+  	valid_addresses.each do |valid_address|
+  		@user.email = valid_address
+  		assert @user.valid?, "#{valid_address} is not valid"
+  	end
+  end
+
+  test "email validation should reject invalid emails" do
+  	invalid_addresses = %w[user@example,com user_at_foo.com user.name@example. foo@bar_baz.com foo@bar+baz.com]
+  	invalid_addresses.each do |invalid_address|
+  		@user.email = invalid_address
+  		assert_not @user.valid?, "#{invalid_address} should be invalid"
+  	end
+  end
+
+  test "email should be unique" do
+  	dupliate_user = @user.dup
+  	@user.save
+  	assert_not dupliate_user.valid?
+  end
+
 end
