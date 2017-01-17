e08867d97322a41f9544ae8b91e9116bf77d5b74
Idyllic <nikamanish007@gmail.com>
2017-01-17 12:10:44 +0530

    Add Index to Email in Users

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -54,4 +54,12 @@ class UserTest < ActiveSupport::TestCase
   	assert_not dupliate_user.valid?
   end
 
+  test "email addresses should be saved as lower-case" do
+  	mixed_case_email = "Foo@ExAmplE.coM"
+  	@user.email = mixed_case_email
+  	@user.save
+  	assert_equal mixed_case_email.downcase, @user.reload.email
+  end
+
 end
+ 
