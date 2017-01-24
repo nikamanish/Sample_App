fc5a6f4b0a8271e66a6bbd480a6679ea4ffec3ea
Idyllic <nikamanish007@gmail.com>
2017-01-24 13:09:28 +0530

    Finish user edit, update,  index, and destroy  actions

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create!(name: "Manish Nikam",
+			 email: "manish@gmail.com",
+			 password: "manish",
+			 password_confirmation: "manish",
+			 admin: true)
+
+99.times do |i|
+	name = Faker::Name.name
+	email = "manish-#{i+1}@gmail.com"
+	password = "manish"
+	User.create!(name: name,
+				 email: email,
+				 password_confirmation: password,
+				 password: password)
+end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -29,14 +29,14 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
 	end
 
 	test "should redirect edit when logged in as wrong user" do
-		log_in_as(@other_user, password: "123456")
+		log_in_as(@other_user, password: "manish")
 		get edit_user_path(@user)
 		assert flash.empty?
 		assert_redirected_to root_url
 	end
 
 	test "should redirect update when logged in as wrong user" do
-		log_in_as(@other_user, password: "123456")
+		log_in_as(@other_user, password: "manish")
 		patch user_path(@user), params: {
 			user: {
 				name: @user.name,
@@ -44,6 +44,33 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
 			}
 		}
 		assert flash.empty?
+		assert_redirected_to root_url
+	end
+
+	test "should redirect index when not logged in" do
+		get users_path
+		assert_redirected_to login_url
+	end
+
+	#excercise
+	test "should not allow admin attribute to be edited via web" do
+		log_in_as(@other_user)
+		assert_not @other_user.admin?
+		patch user_path(@other_user), params: {
+			user: {
+				password: 'manish',
+				password_confirmation: 'manish',
+				admin: 1
+			}
+		}
+		assert_not @other_user.admin?
+	end
+
+	test "should redirect destroy when logged in as non admin" do
+		log_in_as(@other_user)
+		assert_no_difference 'User.count' do
+			delete user_path(@user)
+		end
 		assert_redirected_to root_url
 	end
 end
